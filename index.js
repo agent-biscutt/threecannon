587823f40c0864a6550ec373803299f4bf011dba
agent-biscutt <91116036+agent-biscutt@users.noreply.github.com>
2022-05-15 15:30:16 +1000

    Update index.js
    Woops. Forgot to call world/scene rather than just return the function

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function makeCylinder(radiusTop=1,radiusBottom=1,height=1,mass=1,heightSegments=
 // Creating THREE.js scene and CANNON.js world
 function createWorld(){
   // return a new CANNON world, and new THREE scene as list.
-  return([new CANNON.World,new THREE.Scene]);
+  return([new CANNON.World(),new THREE.Scene()]);
 };
 
 // Add function to add objects created by above functions into world+scene also created by above function
